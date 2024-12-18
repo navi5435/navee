f78d96d467e4a02be2dc2b6c60afe965c3aeb9c0
navi5435 <naveenkumare07@gmail.com>
2024-12-19 00:46:09 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+echo "<h2>Submitted Data (PHP)</h2>"; foreach ($_POST as $key => $value) {
+echo "<p><strong>" . htmlspecialchars($key) . ":</strong> " . htmlspecialchars($value) . "</p>";
+}
+}
+?>
